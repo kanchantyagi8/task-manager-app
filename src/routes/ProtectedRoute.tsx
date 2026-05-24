906acc078be301869995c15d4465832808bf869d
Kanchan Tyagi <kanchant@omnissa.com>
2026-05-24 19:04:29 +0530

    Fixed protected route

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,7 +1,12 @@
+import type { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuthStore } from "../store/authStore";
 
-export default function ProtectedRoute({ children }: any) {
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const token = useAuthStore((s) => s.token);
 
   if (!token) return <Navigate to="/" />;
